c03d735d0d897e1e03077aa6d5bba3696771ccca
nennyfills <nennydike@gmail.com>
2023-03-07 05:54:46 +0100

    buy flow
    - add input and button  component for the buy flow
    - add functions that collects input data
    - add set state for errors
    - add function for the back and next button
    - add styles for the buy flow
    - add type to the summary component
    
    [WIP]

--- src/Pages/BuyFlow/summaryStep.tsx
+++ src/Pages/BuyFlow/summaryStep.tsx
@@ -3,20 +3,27 @@ import { Link } from 'react-router-dom'
 
 interface SummaryStepProps {
   collectedData: {
+    firstName: string
+    lastName: string
     email: string
     age: number
   }
 }
 
-const SummaryStep: React.FC<SummaryStepProps> = (props) => {
+const SummaryStep: React.FC<SummaryStepProps> = ({ collectedData }) => {
   return (
-    <>
-      <div>Email: {props.collectedData.email}</div>
-      <div>Age: {props.collectedData.age}</div>
+    <section>
+      <div>
+        Name: <span>{collectedData.firstName}</span>
+        <span>{collectedData.firstName}</span>
+      </div>
+      <div>Age: {collectedData.lastName}</div>
+      <div>Email: {collectedData.email}</div>
+      <div>Age: {collectedData.age}</div>
       <div>
         <Link to="/purchased=dev_ins">Purchase</Link>
       </div>
-    </>
+    </section>
   )
 }
 
